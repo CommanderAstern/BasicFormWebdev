b7281aad441cd5a860fc08dfa2204e93f9928229
CommanderAstern <45691479+CommanderAstern@users.noreply.github.com>
2021-10-08 22:31:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var name1 = document.getElementById('name1')
+var number = document.getElementById('number')
+var date = document.getElementById('date')
+var mail = document.getElementById('mail')
+var brand = document.getElementById('brand')
+function validate(){
+    let messages = []
+    if (name1.value == '' || (!/[^a-zA-Z]/.test(name1))) {
+        alert("Incorrect Name")   
+    }else if (date.value == '' ) {
+        alert("Incorrect Date")
+    }else if (mail.value == '' ) {
+        alert("Incorrect Mail")
+    }else if (number.value == '' || (!/[^0-9]/.test(number))) {
+        alert("Incorrect Name")   
+    }else if (brand.value == '' ) {
+        alert("Incorrect Brand")
+    }
+
+
+}
+
+
+
+
